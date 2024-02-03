3e00fb9f2cebb3d02fce3eb95785298bc323759a
Razvan Rusu <59891885+Razvan00Rusu@users.noreply.github.com>
2024-02-03 21:05:08 +0000

    [web]: Implement some end-point boilerplate (#8)
    * Implement some end-point boilerplate
    
    * Add create branch end-points
    
    * Add templates for all main endpoints

--- backend/backend.go
+++ backend/backend.go
@@ -5,6 +5,8 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"golang.org/x/time/rate"
+	"io"
+	"log"
 	"net/http"
 )
 
@@ -18,27 +20,99 @@ func StartServer() {
 	)))
 
 	e.GET("/", func(c echo.Context) error {
-		params := map[string]interface{}{
-			"Name": "Claire",
+		return c.Redirect(http.StatusSeeOther, "/branch?bid=main")
+	})
+
+	e.GET("/branch", func(c echo.Context) error {
+		branchId := c.QueryParam("branchId")
+		log.Println("branchID", branchId)
+		if branchId == "" {
+			branchId = "main"
 		}
+		// TODO: Get list of branches and commits
+		branches := []string{"main"}
+		commits := []string{"First Commit"}
+
+		params := map[string]interface{}{
+			"BranchId": branchId,
+			"Branches": branches,
+			"Commits":  commits,
+		}
+
 		return c.Render(http.StatusOK, "index", params)
 	})
 
-	e.POST("/branch/push", func(c echo.Context) error {
-		c.QueryParam("ID")
-		return c.String(http.StatusOK, "branch-push")
+	e.POST("/branch/upload", func(c echo.Context) error {
+		file, err := c.FormFile("file")
+		if err != nil {
+			return err
+		}
+		src, err := file.Open()
+		if err != nil {
+			return err
+		}
+		byteContainer, err := io.ReadAll(src)
+		log.Println(len(byteContainer))
+		return c.String(http.StatusOK, "")
 	})
 
-	e.POST("/checkout/commit", func(c echo.Context) error {
-		return c.String(http.StatusOK, "checkout-commit")
+	e.GET("/branch/new_branch_settings", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "new_branch_settings", nil)
 	})
 
-	e.POST("/branch/new", func(c echo.Context) error {
-		return c.String(http.StatusOK, "branch-new")
+	e.GET("/branch/upload_image_settings", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "upload_image_settings", nil)
 	})
 
-	e.POST("/merge", func(c echo.Context) error {
-		return c.String(http.StatusOK, "merge")
+	e.GET("/branch/merge_branch_settings", func(c echo.Context) error {
+		//TODO: Get branches!!
+		branches := []string{"main"}
+		params := map[string]interface{}{
+			"Branches": branches,
+		}
+		return c.Render(http.StatusOK, "merge_branch_settings", params)
+	})
+
+	e.GET("/branch/merge_preview", func(c echo.Context) error {
+
+		return c.String(http.StatusOK, "")
+	})
+
+	e.POST("/branch/merge_branches", func(c echo.Context) error {
+		mergingFrom := c.FormValue("merging_from")
+		if mergingFrom == "" {
+			return c.String(http.StatusBadRequest, "")
+		}
+		mergingTo := c.FormValue("merging_to")
+		if mergingTo == "" {
+			return c.String(http.StatusBadRequest, "")
+		}
+		// TODO: Create Image Preview!
+		log.Println("Merging from:", mergingFrom, "Merging Into:", mergingTo)
+		return c.String(http.StatusOK, "")
+	})
+
+	e.POST("/branch/create_new_branch", func(c echo.Context) error {
+		branchName := c.FormValue("branch_name")
+
+		if branchName == "" {
+			return c.String(http.StatusBadRequest, "")
+		}
+
+		//TODO: Create new branch
+		log.Println("Create new branch!", branchName)
+		return c.String(http.StatusCreated, "")
+	})
+
+	e.GET("/branch/preview", func(c echo.Context) error {
+		branchId := c.QueryParam("branchId")
+
+		if branchId == "" {
+			return c.String(http.StatusInternalServerError, "Missing branch id")
+		}
+
+		// TODO: Get the image to the client somehow - can be either as a byte array or straight from a file
+		return c.String(http.StatusNotImplemented, "Not implemented yet : )")
 	})
 
 	e.Static("/public", "./frontend/public")
