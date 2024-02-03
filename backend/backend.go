e4129407165d73789e6e466ce77bc0b3991d7463
Razvan Rusu <razvan.rusu19@imperial.ac.uk>
2024-02-03 18:54:51 +0000

    Implement some end-point boilerplate

--- backend/backend.go
+++ backend/backend.go
@@ -5,6 +5,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"golang.org/x/time/rate"
+	"log"
 	"net/http"
 )
 
@@ -18,10 +19,39 @@ func StartServer() {
 	)))
 
 	e.GET("/", func(c echo.Context) error {
-		params := map[string]interface{}{
-			"Name": "Claire",
+		return c.Redirect(http.StatusSeeOther, "/branch?bid=main")
+	})
+
+	e.GET("/branch", func(c echo.Context) error {
+		branchId := c.QueryParam("branchId")
+		log.Println("branchID", branchId)
+		if branchId == "" {
+			branchId = "main"
 		}
+		// TODO: Get list of branches
+		branches := []string{"main"}
+
+		params := map[string]interface{}{
+			"BranchId": branchId,
+			"Branches": branches,
+		}
+
 		return c.Render(http.StatusOK, "index", params)
+	})
+
+	e.GET("/branch/new_branch_settings", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "new_branch_settings", nil)
+	})
+
+	e.GET("/branch/preview", func(c echo.Context) error {
+		branchId := c.QueryParam("branchId")
+
+		if branchId == "" {
+			return c.String(http.StatusInternalServerError, "Missing branch id")
+		}
+
+		// TODO: Get the image to the client somehow - can be either as a byte array or straight from a file
+		return c.String(http.StatusNotImplemented, "Not implemented yet : )")
 	})
 
 	e.POST("/branch/push", func(c echo.Context) error {
