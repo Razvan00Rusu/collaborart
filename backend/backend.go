a807c879201887fbed2883baebc7a5828cdd0125
davidcormier <david.mael.cormier@gmail.com>
2024-02-04 10:44:05 +0000

    send all 4

--- backend/backend.go
+++ backend/backend.go
@@ -131,39 +131,61 @@ func StartServer() {
 	e.POST("/branch/merge_preview", func(c echo.Context) error {
 		mergingFrom := c.FormValue("merging_from")
 		mergingTo := c.FormValue("merging_to")
-		preference := c.FormValue("merge_preference")
 
 		mergedKeepFrom, mergedKeepTo := MergePreview(mergingFrom, mergingTo)
-		targetBranch, _ := vcs.GetBranch(mergingTo)
 
-		var mergedImage composedImage.ComposedImage
+		fromBranch, _ := vcs.GetBranch(mergingFrom)
+		fromImage := composedImage.New(int(fromBranch.Width), int(fromBranch.Height), fromBranch.GetDiffsInBranch())
 
-		if preference == "from" {
-			mergedImage = composedImage.New(
-				int(targetBranch.Width),
-				int(targetBranch.Height),
-				mergedKeepFrom)
-		} else if preference == "to" {
-			mergedImage = composedImage.New(
-				int(targetBranch.Width),
-				int(targetBranch.Height),
-				mergedKeepTo)
-		} else {
-			return c.String(http.StatusBadRequest, "")
-		}
+		toBranch, _ := vcs.GetBranch(mergingTo)
+		toImage := composedImage.New(int(toBranch.Width), int(toBranch.Height), toBranch.GetDiffsInBranch())
 
-		buf := new(bytes.Buffer)
-		if err := png.Encode(buf, &mergedImage.Img); err != nil {
+		keepFromImage := composedImage.New(
+			int(toBranch.Width),
+			int(toBranch.Height),
+			mergedKeepFrom)
+
+		keepToImage := composedImage.New(
+			int(toBranch.Width),
+			int(toBranch.Height),
+			mergedKeepTo)
+
+		bufFrom := new(bytes.Buffer)
+		if err := png.Encode(bufFrom, &fromImage.Img); err != nil {
 			log.Printf("failed to encode: %v", err)
 		}
-		image := buf.Bytes()
-		imgBase64Str := base64.StdEncoding.EncodeToString(image)
+		imageFrom := bufFrom.Bytes()
+		imgBase64StrFrom := base64.StdEncoding.EncodeToString(imageFrom)
 		//fmt.Println("base64 encoding", imgBase64Str)
 
-		params := map[string]interface{}{
-			"Encoding": imgBase64Str,
+		bufTo := new(bytes.Buffer)
+		if err := png.Encode(bufTo, &toImage.Img); err != nil {
+			log.Printf("failed to encode: %v", err)
 		}
-		return c.Render(http.StatusOK, "preview", params)
+		imageTo := bufTo.Bytes()
+		imgBase64StrTo := base64.StdEncoding.EncodeToString(imageTo)
+
+		bufMergeFrom := new(bytes.Buffer)
+		if err := png.Encode(bufMergeFrom, &keepFromImage.Img); err != nil {
+			log.Printf("failed to encode: %v", err)
+		}
+		imageMergeFrom := bufMergeFrom.Bytes()
+		imgBase64StrMergeFrom := base64.StdEncoding.EncodeToString(imageMergeFrom)
+
+		bugMergeTo := new(bytes.Buffer)
+		if err := png.Encode(bugMergeTo, &keepToImage.Img); err != nil {
+			log.Printf("failed to encode: %v", err)
+		}
+		imageMergeTo := bugMergeTo.Bytes()
+		imgBase64StrMergeTo := base64.StdEncoding.EncodeToString(imageMergeTo)
+
+		params := map[string]interface{}{
+			"FromPreview":         imgBase64StrFrom,
+			"ToPreview":           imgBase64StrTo,
+			"FromStrategyPreview": imgBase64StrMergeFrom,
+			"ToStrategyPreview":   imgBase64StrMergeTo,
+		}
+		return c.Render(http.StatusOK, "merge_preview", params)
 	})
 
 	e.POST("/branch/merge_branches", func(c echo.Context) error {
@@ -176,7 +198,12 @@ func StartServer() {
 			return c.String(http.StatusBadRequest, "")
 		}
 		log.Println("Merging from:", mergingFrom, "Merging Into:", mergingTo)
-		Merge(mergingFrom, mergingTo)
+		mergingStrategy := c.FormValue("merge_preference")
+		if mergingStrategy == "from" {
+			Merge(mergingFrom, mergingTo, true)
+		} else {
+			Merge(mergingFrom, mergingTo, false)
+		}
 		c.Response().Header().Add("HX-Refresh", "true")
 		return c.String(http.StatusOK, "")
 	})
